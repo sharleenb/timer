bab3d2160cb81b62314f2d023016e794ca73da6f
Sharleen Beniamin <sharleenb16@gmail.com>
2023-02-20 18:01:17 +0000

    account for edge cases

--- timer1.js
+++ timer1.js
@@ -5,7 +5,11 @@ function compareNumbers(a, b) {
 }
 timerMarks.sort(compareNumbers);
 
+
 for (const arg of timerMarks) {
+  if (isNaN(arg) || arg < 0) {
+    return '';
+  }
   setTimeout(() => {
     process.stdout.write('\x07');
   }, arg * 1000)
